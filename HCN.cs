6a0e9b5dd5a402da64a3e5724824e17829ad19b7
vu giap <vudinhgiap11@gmail.com>
2023-07-19 10:33:39 +0700

    first commit

---
+++ HCN.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lophinhchunhat
+{
+    public class HCN
+    {
+        double width;
+        double height;
+
+        public HCN(){ }
+        public HCN(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double GetArea()
+        {
+            return this.width * this.height;
+        }
+        public double GetPerimeter()
+        {
+            return (this.width + this.height)*2;
+        }
+        public string Display()
+        {
+            return "HCN{" + "width=" + width + ", height=" + height + "}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lophinhchunhat
+{
+    internal class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            HCN hcn1 = new HCN(width, height);
+
+            Console.WriteLine("YOUR HCN \n " + hcn1.Display());
+            Console.WriteLine("chu vi la \n " + hcn1.GetPerimeter());
+            Console.WriteLine("dien tich la \n " + hcn1.GetArea());
+
+        }
+    }
+}
